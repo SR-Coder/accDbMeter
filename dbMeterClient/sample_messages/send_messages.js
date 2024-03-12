03c87f43e554cc07cb5eb55482ae4b6b76a9ff2a
Rob Dawson <rob.dawson@consensys.net>
2024-03-11 18:47:19 -0700

    Created initial test harness to send messages .

---
+++ dbMeterClient/sample_messages/send_messages.js
@@ -0,0 +1,26 @@
+import mqtt from 'mqtt';
+const sendMessageInterval = 100;
+const sensors = [
+    { "sensorId": 12, "sensorName": "Front of Church" },
+    { "sensorId": 23, "sensorName": "Middle of Church" },
+    { "sensorId": 42, "sensorName": "Back of Church" },
+];
+let topic = "dbMeter/dbLevel";
+let client = mqtt.connect("mqtt://localhost:1885");
+setInterval(publishMessageForEachSensor, sendMessageInterval);
+
+function publishMessageForEachSensor() {
+    sensors.forEach((sensor) => {
+        let message = {};
+        message.dbLevel = Math.random() * 20 + 60;
+        message.timestamp = new Date().getTime();
+        message.sensorId = sensor.sensorId;
+        message.sensorName = sensor.sensorName;
+        console.log("publishing", message);
+        client.publish(topic, JSON.stringify(message), { qos: 2 }, (error) => {
+            if (error) {
+                console.log('Publish error: ', error)
+            }
+        });
+    });
+}
