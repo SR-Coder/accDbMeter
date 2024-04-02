a85ecc74241f741f87e23bf38c67e78f2d0304b4
SR-Coder <reederje@gmail.com>
2024-04-02 08:46:39 -0700

    Update sensor configuration and styling

--- sensor/esp32-s3-DevKitC/src/main.cpp
+++ sensor/esp32-s3-DevKitC/src/main.cpp
@@ -2,6 +2,9 @@
 #define SPIFFS LittleFS
 #endif
 
+#define LED LED_BUILTIN
+#define rLED 
+
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <LittleFS.h>
@@ -10,6 +13,8 @@
 #include <HardwareSerial.h>
 #include <WiFi.h> 
 #include <PubSubClient.h>
+
+// #include "processor.h"
 
 // FILESYSTEM STUFF
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
@@ -49,14 +54,77 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 char mqtt_name[128];
 char mqtt_server[128];
+int mqtt_rate;
+bool run_mqtt = true;
 
 // Create a instance of the WifiManager
 WIFIMANAGER WifiManager;
-
 AsyncWebServer webServer(80);
+
+// create structure to hold file data
+struct config_t {
+    String sensor_name;
+    String sensor_username;
+    String sensor_password;
+    String sensor_location_name;
+    String units;
+    int x_loc;
+    int y_loc;
+    String mqtt_server;
+    int mqtt_port;
+    String mqtt_username;
+    String mqtt_password;
+    int mqtt_rate;
+}; 
+
+config_t my_config;
+
+String processor(const String& var){
+    if(var == "SENSOR_NAME"){
+        return String(my_config.sensor_name);
+    }
+    if(var == "USER_NAME"){
+        return String(my_config.sensor_username);
+    }
+    if(var == "SENSOR_PASSWORD"){
+        return String(my_config.sensor_password);
+    }
+    if(var == "LOCATION_NAME"){
+        return String(my_config.sensor_location_name);
+    }
+    if(var == "UNITS"){
+        return String(my_config.units);
+    }
+    if(var == "X_LOC"){
+        return String(my_config.x_loc);
+    }
+    if(var == "Y_LOC"){
+        return String(my_config.y_loc);
+    }
+    if(var == "MQTT_SERVER"){
+        return String(my_config.mqtt_server);
+    }
+    if(var == "MQTT_PORT"){
+        return String(my_config.mqtt_port);
+    }
+    if(var == "MQTT_USER"){
+        return String(my_config.mqtt_username);
+    }
+    if(var == "MQTT_PASSWORD"){
+        return String(my_config.mqtt_password);
+    }
+    if(var == "MQTT_RATE"){
+        return String(my_config.mqtt_rate);
+    }
+    return String();
+}
 
 void setup()
 {
+
+  // Setup the pins for the led
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
 
   Serial.begin(115200);
   Serial.println("Connected");
@@ -89,6 +157,26 @@ void setup()
   strncpy(mqtt_name, doc["mqtt_name"], sizeof(mqtt_name));
   mqtt_name[sizeof(mqtt_name) - 1] = '\0';
 
+  const char* username = doc["username"];
+  const char* password = doc["password"];
+  mqtt_rate = doc["mqtt_rate"].as<int>();
+
+  // add values to struct
+  my_config.sensor_name = doc["sensor_name"].as<String>();
+  my_config.sensor_username = doc["sensor_username"].as<String>();
+  my_config.sensor_password = doc["sensor_password"].as<String>();
+  my_config.sensor_location_name = doc["sensor_location_name"].as<String>();
+  my_config.units = doc["units"].as<String>();
+  my_config.x_loc = doc["x_loc"].as<int>();
+  my_config.y_loc = doc["y_loc"].as<int>();
+  my_config.mqtt_server = doc["mqtt_server"].as<String>();
+  my_config.mqtt_port = doc["mqtt_port"].as<int>();
+  my_config.mqtt_username = doc["mqtt_username"].as<String>();
+  my_config.mqtt_password = doc["mqtt_password"].as<String>();
+  my_config.mqtt_rate = doc["mqtt_rate"].as<int>();
+
+
+
 // WIFI MANAGER SETUP
   WifiManager.startBackgroundTask();
   WifiManager.fallbackToSoftAp();
@@ -98,6 +186,7 @@ void setup()
   webServer
     .serveStatic("/", SPIFFS, "/www")
     .setDefaultFile("index.html")
+    .setAuthentication(username, password)
     .setFilter(ON_STA_FILTER);
 
   webServer
@@ -143,10 +232,26 @@ void setup()
   });
 
   webServer.on("/restart", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/plain", "Resetting");
     delay(1000);
+    request->redirect("/");
     ESP.restart();
   });
+
+  webServer.on("/mqttStop", HTTP_GET, [](AsyncWebServerRequest *request){
+    run_mqtt = false;
+    request->redirect("/");
+  });
+
+  webServer.on("/mqttStart", HTTP_GET, [](AsyncWebServerRequest *request){
+    run_mqtt = true;
+    request->redirect("/");
+  });
+
+  // Serve the Favicon file
+  webServer
+    .serveStatic("/favicon.ico", SPIFFS, "/www")
+    .setDefaultFile("favicon.ico");
+    // .setFilter(ON_STA_FILTER);
 
   // Server static files for the AP
   webServer
@@ -216,17 +321,38 @@ void reconnect(){
 
 }
 
+bool isOn = false;
+// LED Functions
+void ledOn(){
+  neopixelWrite(RGB_BUILTIN, 0,100,0);
+  isOn = true;
+}
+void ledOff(){
+  neopixelWrite(RGB_BUILTIN, 0,0,0);
+  isOn = false;
+}
+
+void ledRed(){
+  neopixelWrite(RGB_BUILTIN, 100,0,0);
+}
+
 void loop() 
 {
   // put your main code here, to run repeatedly:
 
   String msg = "Message Number: " + String(counter);
 
-  if(client.connected()){
+  if(client.connected() && run_mqtt){
+    if(isOn){
+      ledOff();
+    } else {
+      ledOn();
+    };
     client.publish("test", msg.c_str());
     counter++;
   } else {
     Serial.println("Client not connected");
+    ledRed();
     reconnect();
   }
 
@@ -234,13 +360,8 @@ void loop()
     client.loop();
     lastTime = millis();
   }
+  // SETS HOW OFTEN THE MQTT CLIENT WILL SEND A MESSAGE
+  delay(mqtt_rate);
 
-
-
-  delay(50);
-
-  if (false) {
-    WifiManager.runSoftAP();
-  }
 }
 
